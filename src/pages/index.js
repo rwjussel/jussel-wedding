fa9faf6265f12bd7d6e3c3d09ed957b58c960d82
Ryan Jussel <rjussel@newrelic.com>
2021-11-25 17:02:28 -0600

    Added fave photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Jussel Wedding">
       <p>May 28, 2022 -- save the date!</p>
+      <StaticImage
+        alt="Ryan & Jillian Hanging out in Big Sur Monterey trip"
+        src="../images/BigSurWeddingShot.jpg"
+        />
     </Layout>
   )
 }
