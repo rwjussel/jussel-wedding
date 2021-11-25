f6b048f62d4f5cb323f2aa28db75bc549dade76a
Ryan Jussel <rjussel@newrelic.com>
2021-11-25 14:05:19 -0600

    Initial Commit; - just finished gatsby tutorial part 2

---
+++ src/components/layout.js
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText
+ } from './layout.module.css'
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <title>{pageTitle}</title>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>Wedding</Link>
+              </li>
+          <li className={navLinkItem}>
+              <Link to="/story" className={navLinkText}>Our Story</Link>
+              </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/story.js
@@ -0,0 +1,20 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="The Story">
+    <p>May 28, 2022 -- save the date!</p>
+    <p> A story over 20 years in the making... </p>
+    <p>Here is info about the lovely couple.</p>
+
+  </Layout>
+     
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
