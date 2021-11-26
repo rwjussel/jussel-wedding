27b9686f46e8fadadf9df152349a34fe1f01eb20
Ryan Jussel <rjussel@newrelic.com>
2021-11-26 10:03:37 -0600

    Updated page Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Jussel Wedding">
+    <Layout pageTitle="Ryan & Jillian">
       <p>May 28, 2022 -- save the date!</p>
       <StaticImage
         alt="Ryan & Jillian Hanging out in Big Sur Monterey trip"
